a3a5845ce3bb6534498b5af3bb578d3c42b27b26
wls <jcwum@isoftstone.com>
2020-07-23 16:01:33 +0800

    2020/7/23----srcoll

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -46,7 +46,7 @@ class _MyHomePageState extends State<MyHomePage>
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("剑客"),
+        title: Text(""),
       ),
       body: new PageView(
         controller: _pageController,
---
+++ lib/widgets/converse_list_header.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class ConverseListHeader extends StatelessWidget {
+  ConverseListHeader({this.onPressed});
+
+  final VoidCallback onPressed;
+
+  ///get 方法, 不需要() 格式
+  String get showText => "当前登录";
+
+  @override
+  Widget build(BuildContext context) {
+    return InkResponse(
+        onTap: onPressed,
+        child: Container(
+          padding: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            color: Colors.grey,
+            border: Border(
+              bottom: BorderSide(
+                width: 2,
+                color: Colors.green,
+              ),
+            ),
+          ),
+          child: Row(
+            children: <Widget>[
+              Image.asset(
+                "asserts/images/Icon_Mac.png",
+                width: 40,
+                height: 40,
+                color: Colors.white,
+              ),
+              Container(
+                width: 10,
+              ),
+              Text(
+                this.showText,
+                style: TextStyle(fontSize: 17, color: Colors.white),
+              )
+            ],
+          ),
+        ));
+  }
+}
